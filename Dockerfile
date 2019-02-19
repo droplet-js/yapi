6888d1ea3c91ed5fb01c1465d8e35327bff4a77d
v7lin <v7lin@qq.com>
2019-02-19 14:42:26 +0800

    yapi 1.5.2

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,15 @@ ENV YAPI_VERSION 1.5.2
 RUN wget https://github.com/YMFE/yapi/archive/v${YAPI_VERSION}.tar.gz
 RUN tar xzf v${YAPI_VERSION}.tar.gz
 
+# 魔改
+COPY server/yapiEnv.js /yapi-${YAPI_VERSION}/server
+RUN sed -i "s|yapi.commons.generatePassword('ymfe.org', passsalt)|yapi.commons.generatePassword(yapi.WEBCONFIG.adminPassword, passsalt)|g" /yapi-${YAPI_VERSION}/server/install.js
+RUN sed -i "s|const config = require('../../config.json');|const yapiEnv = require('./yapiEnv.js');\r\nconst config = yapiEnv.parseConfig();|g" /yapi-${YAPI_VERSION}/server/yapi.js
+RUN sed -i "s|yapi.path.join(yapi.WEBROOT_RUNTIME, 'init.lock')|yapi.path.join(yapi.WEBROOT_RUNTIME, 'init', 'init.lock')|g" /yapi-${YAPI_VERSION}/server/install.js
+
+RUN cat /yapi-${YAPI_VERSION}/server/install.js
+RUN cat /yapi-${YAPI_VERSION}/server/yapi.js
+
 # --- prod ---
 
 FROM node:7.6-alpine as prod
@@ -29,4 +38,4 @@ WORKDIR /yapi/vendors
 
 EXPOSE 3000
 
-CMD ["/bin/sh", "-c", "if [ ! -e \"/yapi/init/init.lock\" ]; then npm run install-server; cp /yapi/init.lock /yapi/init/; fi; ykit pack -m; node server/app.js"]
+CMD ["/bin/sh", "-c", "if [ ! -e \"/yapi/init/init.lock\" ]; then npm run install-server; fi; ykit pack -m; node server/app.js"]
---
+++ server/yapiEnv.js
@@ -0,0 +1,38 @@
+exports.parseConfig() {
+    return {
+        port: process.env.YAPI_PORT,
+        closeRegister: process.env.YAPI_CLOSEREGISTER === 'true',
+        versionNotify: process.env.YAPI_VERSIONNOTIFY === 'true',
+        adminAccount: process.env.YAPI_ADMINACCOUNT,
+        adminPassword: process.env.YAPI_ADMINPASSWORD,
+        db: {
+            connectString: process.env.YAPI_DB_CONNECTSTRING,
+            servername: process.env.YAPI_DB_SERVERNAME,
+            port: parseInt(process.env.YAPI_DB_PORT),
+            DATABASE: process.env.YAPI_DB_DATABASE,
+            user: process.env.YAPI_DB_USER,
+            pass: process.env.YAPI_DB_PASS
+        },
+        mail: {
+            enable: process.env.YAPI_MAIL_ENABLE === 'true',
+            host: process.env.YAPI_MAIL_HOST,
+            port: parseInt(process.env.YAPI_MAIL_PORT),
+            from: process.env.YAPI_MAIL_FROM,
+            auth: {
+                user: process.env.YAPI_MAIL_AUTH_USER,
+                pass: process.env.YAPI_MAIL_AUTH_PASS
+            }
+        },
+        ldapLogin: {
+            enable: process.env.YAPI_LDAPLOGIN_ENABLE === 'true',
+            server: process.env.YAPI_LDAPLOGIN_SERVER,
+            baseDn: process.env.YAPI_LDAPLOGIN_BASEDN,
+            bindPassword: process.env.YAPI_LDAPLOGIN_BINDPASSWORD,
+            searchDn: process.env.YAPI_LDAPLOGIN_SEARCHDN,
+            searchStandard: process.env.YAPI_LDAPLOGIN_SEARCHSTANDARD,
+            emailPostfix: process.env.YAPI_LDAPLOGIN_EMAILPOSTFIX,
+            emailKey: process.env.YAPI_LDAPLOGIN_EMAILKEY,
+            usernameKey: process.env.YAPI_LDAPLOGIN_USERNAMEKEY
+        }
+    };
+};
