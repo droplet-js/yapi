8f2791b0ff21efbc9f1b876719a292b9ab552f5b
v7lin <v7lin@qq.com>
2019-02-19 14:55:22 +0800

    yapi test

--- server/yapiEnv.js
+++ server/yapiEnv.js
@@ -1,8 +1,8 @@
 exports.parseConfig() {
     return {
         port: process.env.YAPI_PORT,
-        closeRegister: process.env.YAPI_CLOSEREGISTER === 'true',
-        versionNotify: process.env.YAPI_VERSIONNOTIFY === 'true',
+        closeRegister: process.env.YAPI_CLOSEREGISTER == 'true' ? true : false,
+        versionNotify: process.env.YAPI_VERSIONNOTIFY == 'true' ? true : false,
         adminAccount: process.env.YAPI_ADMINACCOUNT,
         adminPassword: process.env.YAPI_ADMINPASSWORD,
         db: {
@@ -14,7 +14,7 @@ exports.parseConfig() {
             pass: process.env.YAPI_DB_PASS
         },
         mail: {
-            enable: process.env.YAPI_MAIL_ENABLE === 'true',
+            enable: process.env.YAPI_MAIL_ENABLE == 'true' ? true : false,
             host: process.env.YAPI_MAIL_HOST,
             port: parseInt(process.env.YAPI_MAIL_PORT),
             from: process.env.YAPI_MAIL_FROM,
@@ -24,7 +24,7 @@ exports.parseConfig() {
             }
         },
         ldapLogin: {
-            enable: process.env.YAPI_LDAPLOGIN_ENABLE === 'true',
+            enable: process.env.YAPI_LDAPLOGIN_ENABLE == 'true' ? true : false,
             server: process.env.YAPI_LDAPLOGIN_SERVER,
             baseDn: process.env.YAPI_LDAPLOGIN_BASEDN,
             bindPassword: process.env.YAPI_LDAPLOGIN_BINDPASSWORD,
