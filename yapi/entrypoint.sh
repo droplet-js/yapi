af06fa8c0eecacffe5f57b88b8b67db34acb3382
v7lin <v7lin@qq.com>
2019-02-20 10:57:41 +0800

    yapi test

--- yapi/entrypoint.sh
+++ yapi/entrypoint.sh
@@ -2,6 +2,6 @@
 
 ./wait-for-it.sh ${YAPI_DB_SERVERNAME}:${YAPI_DB_PORT} -- echo "yapi db is up";
 
-if [! -e "/yapi/init/init.lock"]; then npm run install-server; fi;
+if [ ! -e "/yapi/init/init.lock" ]; then npm run install-server; fi;
 
 node server/app.js;
